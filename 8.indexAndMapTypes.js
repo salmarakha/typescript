71e55a515e67cc97e23610083080049260d14b62
salmarakha <61853942+salmarakha@users.noreply.github.com>
2022-04-13 12:02:04 +0200

    add index and type maps

--- 8.indexAndMapTypes.js
+++ 8.indexAndMapTypes.js
@@ -1,37 +1,21 @@
-// 1. Index Type
-interface List {
-    id: number;
-    title: string;
-}
-// They look the same as accessing a property of an object but refer to types.
-type IdType = List['id']; // number
 // We sometimes refer to index types as lookup types. (lookup types is common)
-
-const list1: List = {
+var list1 = {
     id: 15,
     title: 'Something'
 };
-
 // this function returns the value of the passed property of an object
-function getProperty<ObjectType, KeyType extends keyof ObjectType>(
-    object: ObjectType,
-    property: KeyType
-): ObjectType[KeyType] {
+function getProperty(object, property) {
     return object[property];
 }
-
 // When we use our  getProperty, the compiler checks if a string that we pass into it is an actual property of an object.
 // const value = getProperty(list1, 'property');
-const value1 = getProperty(list1, 'title');
-
+var value1 = getProperty(list1, 'title');
 // When we return  object[property], the TypeScript compiler performs a type lookup. => User[property] 
 // Thanks to that, the return type of the  getProperty varies based on the passed string.
 // getProperty(list1, 'id').toLowerCase(); // Property 'toLowerCase' does not exist on type 'number'.
 getProperty(list1, 'title').toLowerCase();
-
 // 2. Creating a Map from an object
 // A real-life example of the above might be converting an object to a Map. 
-/* The Map object holds key-value pairs and remembers the original insertion order of the keys. 
+/* The Map object holds key-value pairs and remembers the original insertion order of the keys.
     Any value (both objects and primitive values) may be used as either a key or a value. */
 // TypeScript aside, the most straightforward way to do this is to use Object.entries.
-
