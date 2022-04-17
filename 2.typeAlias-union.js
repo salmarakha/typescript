06b7072bdf08968cba9caeb030c0521ab4a743b7
salmarakha <61853942+salmarakha@users.noreply.github.com>
2022-04-17 16:04:54 +0200

    type modify type-alias

--- 2.typeAlias-union.js
+++ 2.typeAlias-union.js
@@ -1,20 +1,90 @@
-// TYPE DEFINITIONS
-// variables
+/** TYPE DEFINITIONS
+ * variables
+*/
 var mood = "happy";
 var score = 5;
+var username = "salmarakha";
 // score = "6" // compiler error
-var values = ["foo", "bar"]; // array
+var isEmpty = true;
+// Array and Tuple
+var values = ["foo", "bar"];
+var arr = [5, 10, 20];
 var colors = ['#ff0000', '#00ff00', '#0000ff']; // tuple (fixed size and data types)
-// functions 
+// Typescript special types
+// any
+// that you can use whenever you don’t want a particular value to cause typechecking errors.
+var object1 = { x: 0 };
+// None of the following lines of code will throw compiler errors.
+// object1.foo();
+// object1();
+// object1.bar = 100;
+// object1 = "hello";
+// const n: number = object1;
+// unknown
+// Any value can be assigned to a variable of type unknown. This means that unknown is a supertype of every other type.
+var x;
+x = 10;
+x = "play";
+var y;
+y = 15;
+y = "some string";
+/** functions
+ * you can skip the difinition of the return type
+*/
+// expression function with arrow function
 var getFullname = function (firstname, lastname) {
     return firstname + " " + lastname;
 };
 console.log(getFullname("Salma", "Rakha"));
-// UNION 
-// union is used as 'OR' but not in a logical way
-var color = null; // default null (always use a default value instead of undefined)
-// allow arrays to accept multiple types
-var user = ["Ben", 18];
+console.log(typeof getFullname); // 'function'
+// TYPE ALIAS, UNION AND INTERSECTION
+/** UNION
+ * union is used to allow multiple types
+ * ex. default null (always use a default value instead of undefined, undefined is JS's default)
+*/
+var color = null;
+var user = ["Ben", 18]; // allow arrays to accept multiple types
 var myColor = ['#ff0000', '#00ff00', '#0000ff'];
 // TYPE ALIAS + UNION
 var colour = null;
+var role = 'admin';
+var client = {
+    id: 1,
+    name: "Hamam",
+    hobbies: ["reading", "football"],
+    street: "Some street",
+    city: "Amsterdam"
+};
+function getSomeValue() {
+    // some logic
+    return 5;
+}
+// Enums
+// Enums are a feature added to JavaScript by TypeScript.
+// Enums allow a developer to define a set of named constants. 
+// TypeScript provides both numeric and string-based enums.
+var UserResponse;
+(function (UserResponse) {
+    UserResponse[UserResponse["No"] = 0] = "No";
+    UserResponse[UserResponse["Yes"] = 1] = "Yes";
+})(UserResponse || (UserResponse = {}));
+function respond(recipient, message) {
+    // ...
+    console.log(message);
+}
+respond("Princess Caroline", UserResponse.Yes);
+// Up is initialized with 1. All of the following members are auto-incremented from that point on. default starts from 0.
+// enum with computed values
+var E;
+(function (E) {
+    E[E["B"] = 0] = "B";
+    E[E["A"] = getSomeValue()] = "A";
+})(E || (E = {}));
+// string based
+var DIRECTION;
+(function (DIRECTION) {
+    DIRECTION["Up"] = "UP";
+    DIRECTION["Down"] = "DOWN";
+    DIRECTION["Left"] = "LEFT";
+    DIRECTION["Right"] = "RIGHT";
+})(DIRECTION || (DIRECTION = {}));
--- 2.typeAlias-union.ts
+++ 2.typeAlias-union.ts
@@ -3,18 +3,46 @@
 */
 const mood = "happy";
 let score: number = 5;
+let username: string = "salmarakha";
 // score = "6" // compiler error
+let isEmpty: boolean = true;
+
+// Array and Tuple
 let values: string[] = ["foo", "bar"]; 
+let arr: Array<number> = [5, 10, 20]; 
 let colors: [string, string, string] = ['#ff0000', '#00ff00', '#0000ff']; // tuple (fixed size and data types)
+
+// Typescript special types
+// any
+// that you can use whenever you don’t want a particular value to cause typechecking errors.
+let object1: any = { x: 0 };
+// None of the following lines of code will throw compiler errors.
+// object1.foo();
+// object1();
+// object1.bar = 100;
+// object1 = "hello";
+// const n: number = object1;
+
+// unknown
+// Any value can be assigned to a variable of type unknown. This means that unknown is a supertype of every other type.
+let x: unknown;
+x = 10;
+x = "play";
+let y: any;
+y = 15;
+// let z: number = y
+y = "some string";
 
 
 /** functions
  * you can skip the difinition of the return type
 */
+// expression function with arrow function
 const getFullname = (firstname: string, lastname: string): string => {
     return firstname + " " + lastname;
 }
 console.log(getFullname("Salma", "Rakha"));
+console.log(typeof getFullname); // 'function'
 
 // TYPE ALIAS, UNION AND INTERSECTION
 /** UNION 
@@ -22,20 +50,22 @@ console.log(getFullname("Salma", "Rakha"));
  * ex. default null (always use a default value instead of undefined, undefined is JS's default)
 */
 let color: [string, string, string] | null = null; 
-let user: (string | number)[] = ["Ben", 18]; // allow arrays to accept multiple types
-
-// String literal types are often used with unions. 
-// A string literal can only be assigned a particular string value. It can be considered a subtype of a string type.
-type UserRole = 'admin' | 'moderator' | 'author';
+let user: (string | number)[] = ["Ben", "18"]; // allow arrays to accept multiple types
 
 /** TYPE ALIAS
  * what if we want to reuse the color variable type or give it a meaningful name:
-*/
+ */
 type PrimaryColors = [string, string, string];
 let myColor: PrimaryColors = ['#ff0000', '#00ff00', '#0000ff'];
 
 // TYPE ALIAS + UNION
 let colour: PrimaryColors | null = null;
+
+// String Literals
+// String literal types are often used with unions. 
+// A string literal can only be assigned a particular string value. It can be considered a subtype of a string type.
+type UserRole = 'admin' | 'moderator' | 'author';
+let role: UserRole = 'admin';
 
 /** TYPE ALIAS + INTERSECTION
  * intersection is used to combine multiple types
@@ -59,4 +89,36 @@ let client: Client = {
     city: "Amsterdam"
 }
 
+function getSomeValue() {
+    // some logic
+    return 5;
+}
 
+// Enums
+// Enums are a feature added to JavaScript by TypeScript.
+// Enums allow a developer to define a set of named constants. 
+// TypeScript provides both numeric and string-based enums.
+enum UserResponse {
+    No =1,
+    Yes  //2
+}
+
+function respond(recipient: string, message: UserResponse): void {
+    // ...
+    console.log(message); //2
+}
+respond("Princess Caroline", UserResponse.Yes);
+
+// Up is initialized with 1. All of the following members are auto-incremented from that point on. default starts from 0.
+// enum with computed values
+enum E {
+    B, // = 0 by defaul
+    A = getSomeValue(),
+}
+// string based
+enum Direction {
+    Up = "UP",
+    Down = "DOWN",
+    Left = "LEFT",
+    Right = "RIGHT",
+}
